ad60be210557c00ae2a3c3039645b6e53b6102cd
nek0m0dok1 <tokuyoshi2199wk@gmail.com>
2026-08-10 16:18:15 +0900

    fix/22-version(2)のtasksの指定をversion(1)と揃える

--- src/database/db.ts
+++ src/database/db.ts
@@ -18,7 +18,7 @@ export class TaskWithFormDatabase extends Dexie {
     })
     this.version(2).stores({
       tasks:
-        'id, &externalKey, source, courseId, courseName, courseWorkId, subjectName, dueDate, status, [status+dueDate], submittedAt',
+        'id, &externalKey, courseId, subjectName, dueDate, status, [status+dueDate]',
       syncStates: 'courseId',
       answerConfirmations: '++id, formUrl, status, submittedAt',
     })
